7a08a147c56abbf53e8bf2a547dcc7c99e65b358
relam10 <alejandro2210lp@gmail.com>
2021-06-20 20:51:19 +0200

    Starting project"

---
+++ index.js
@@ -0,0 +1,13 @@
+const button = document.querySelector('.impossible-button')
+
+const randomNumber = () => {
+    return Math.floor(Math.random()*100)
+}
+
+
+button.addEventListener('mouseover', () => {
+    button.style.top = randomNumber() + '%'
+    button.style.left = randomNumber() + '%'
+    console.log(randomNumber());
+})
+
